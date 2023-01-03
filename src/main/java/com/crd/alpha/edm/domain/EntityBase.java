2e434c4662af992e2b139892181822aab4b45f6f
martin <msmith1961@mac.com>
2023-01-02 20:04:26 -0500

    interm mapper work

--- src/main/java/com/crd/alpha/edm/domain/EntityBase.java
+++ src/main/java/com/crd/alpha/edm/domain/EntityBase.java
@@ -13,18 +13,44 @@ import java.util.Map;
 @Getter
 public abstract class EntityBase {
 
+    /**
+     * enotuty name. this is usully the root table for the enotity e.g. EDM_Issuer
+     */
     protected String name;
+
+    /**
+     * list of ny one to many tables associated with the entity.
+     * all one ot many table will have an associated List<EntityType> property
+     * in the entity.
+     */
     private final List<String> oneToManyTables = new ArrayList<>();
+
+    /**
+     * associates the tables involved in creating this entity with column names
+     * used in the entoty
+     */
     private final Map<String, List<String>> tableColumnMap = new HashMap<>();
     /*
     look into need to handle new roperties dynamically.
     private final Map<String, Object> addedProperties = new HashMap<>();
 */
+    /**
+     * maps primary key columns to all tables involved in creating this entoty
+     */
     private final Map<String, List<String>> tablePrimryKeyColumnMap = new HashMap<>();
 
+    /**
+     * map of entity properties to their associated database column
+     */
     private static final Map<String, String> columnsCamelToSnakeMap = new HashMap<>();
+    /**
+     * map of database columns to theie associated entity property.
+     */
     private static final Map<String, String> columnsSnakeToCamelMap = new HashMap<>();
 
+    /**
+     * calls setTableColumnMap method to instantiate core data.
+     */
     public EntityBase() {
         setTableColumnMap();
     }
@@ -33,7 +59,13 @@ public abstract class EntityBase {
         this.name = name;
         setTableColumnMap();
     }
+
+    /**
+     * this method must be overrideen to correctly set up the core data for database persistence
+     * if should set the entity name and add data using addTableColumn
+     */
     protected abstract void setTableColumnMap();
+
     public List<String> getOneToManyTables() {
         return java.util.Collections.unmodifiableList(oneToManyTables);
     }
@@ -56,10 +88,33 @@ public abstract class EntityBase {
         tablePrimryKeyColumnMap.get(table).add(column);
     }
 
+    /**
+     * adds a database column to columnsCamelToSnakeMap and columnsSnakeToCamelMap
+     * using the default toCammelCase method from CaseUtils. If an override is required
+     * the addOverridetoCaseMaps method must be called after this method is called for the
+     * relevant column.
+     * @param column
+     */
     private void addToCaseMaps(String column) {
         String camel = CaseUtils.toCamelCase(column,false,'_');
         columnsCamelToSnakeMap.putIfAbsent(camel, column);
         columnsSnakeToCamelMap.putIfAbsent(column, camel);
+    }
+
+    /**
+     * overrides the default snake to camel mapping with a custom value.
+     * the existing default mapping will be removed from columnsCamelToSnakeMap
+     * and columnsSnakeToCamelMap.
+     *
+     * @param snakeColumn snake case column name.
+     * @param camelColumn override value.
+     */
+    private void addOverridetoCaseMaps(String snakeColumn, String camelColumn){
+        String toRemove = CaseUtils.toCamelCase(camelColumn,false,'_');
+        columnsCamelToSnakeMap.remove(toRemove);
+        columnsCamelToSnakeMap.putIfAbsent(camelColumn, snakeColumn);
+        columnsSnakeToCamelMap.putIfAbsent(snakeColumn, camelColumn);
+
     }
 
     public String snakeColumnFromCamel(String camel){
@@ -69,6 +124,12 @@ public abstract class EntityBase {
         return columnsSnakeToCamelMap.get(snake);
     }
 
+    /***
+     * returns the primary key columns for the table.
+     *
+     * @param table
+     * @return
+     */
     public List<String> getKeyColumns(String table){
         return tablePrimryKeyColumnMap.get(table);
     }
